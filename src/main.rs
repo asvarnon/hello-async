a5f30cd1f6d38abdf5df19305d24a0cc72ae9a5a
Austin Varnon <austinvarnon@gmail.com>
2026-04-25 12:53:15 -0500

    Implement async page title fetching in main function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use trpl::Html;
 
 fn main() {
-    
+    let args: Vec<String> = std::env::args().collect();
+
+    trpl::block_on(async {
+        let url = &args[1];
+        match page_title(url).await {
+            Some(title) => println!("The title for {url} was {title}"),
+            None => println!("{url} had no title"),
+        }
+    })
 }
  async fn page_title(url: &str) -> Option<String> {
     let response_text = trpl::get(url).await.text().await;
